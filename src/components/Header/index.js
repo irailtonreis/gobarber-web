ac99d976b4d8a546b541bd458e2f30ddbb2aa833
irailtonreis <irailton.oliveira91@gmail.com>
2020-05-09 17:32:47 -0300

    feat: estilo notificações

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Container, Content, Profile } from './styles';
 
+import Notifications from '~/components/Notifications';
+
 import logo from '~/assets/logo-purple.svg';
 
 export default function Header() {
@@ -13,6 +15,7 @@ export default function Header() {
           <Link to="/dashboard">DASHBOARD</Link>
         </nav>
         <aside>
+          <Notifications />
           <Profile>
             <div>
               <strong>Irailton Reis </strong>
