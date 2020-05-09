96995adf45e37e50cbba088a3d24f34ecae8c901
irailtonreis <irailton.oliveira91@gmail.com>
2020-05-09 15:48:45 -0300

    feat: adicionando header

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -60,7 +60,7 @@ export function setToken({ payload }) {
   }
 }
 export default all([
-  takeLatest('persit/REHYDRATE', setToken),
+  takeLatest('persist/REHYDRATE', setToken),
   takeLatest('@auth/SIGN_IN_REQUEST', signIn),
   takeLatest('@auth/SIGN_UP_REQUEST', signUp),
 ]);
